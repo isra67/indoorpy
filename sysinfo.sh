b515e043b1c9ad994a2bbc0fc0f0db3851a14533
isra67 <isramko@gmail.com>
2017-02-16 14:53:14 +0100

    sysinfo.sh - bugfix

--- sysinfo.sh
+++ sysinfo.sh
@@ -3,8 +3,8 @@ SERIAL=`cat /proc/cpuinfo | grep -i '^serial' | grep -Eo '[a-fA-F0-9]{16}$'`
 DAT=$(date "+%Y-%m-%d")
 IP_ADDR=`hostname -I`
 INET=`cat /etc/network/interfaces | grep "eth0 inet" | awk '{print $4}'`
-NETMASK=`ifconfig eth0 | grep inet | awk '{print $4}' | sed 's/Mask://'`
-BROADCAST=`ifconfig eth0 | grep inet | awk '{print $3}' | sed 's/Bcast://'`
+NETMASK=`ifconfig eth0 | grep "inet addr" | awk '{print $4}' | sed 's/Mask://'`
+BROADCAST=`ifconfig eth0 | grep "inet addr" | awk '{print $3}' | sed 's/Bcast://'`
 GATEWAY=`netstat -rn | grep eth0 | awk '{if($1=="0.0.0.0") print $2}'`
 NETWORK=`netstat -rn | grep eth0 | awk '{if($1!="0.0.0.0") print $1}'`
 DNS=`cat /etc/resolv.conf | grep nameserver | awk 'NR==1 {print $2}'`

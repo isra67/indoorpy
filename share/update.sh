f3756778acfaa59f30232495e552a849036ae0c1
isra67 <isramko@gmail.com>
2017-06-28 09:11:23 +0200

    bugfix - update

--- share/update.sh
+++ share/update.sh
@@ -21,8 +21,9 @@ cp -f sounds/ring_* backups
 
 ## synchronize
 #git fetch --all
-git fetch https://isra67:eloeii3769@github.com/isra67/indoorjs.git
-git reset --hard origin/master
+git fetch https://isra67:eloeii3769@github.com/isra67/indoorpy.git
+#git reset --hard origin/master
+git reset --hard gh/master
 git clean -dn
 
 ## remove unnecessary files

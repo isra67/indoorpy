ad8216d6dad1169efe770495ec5042f2118156e0
isra67 <isramko@gmail.com>
2017-09-27 07:30:51 +0200

    update scripts - testing3

--- appdiff.sh
+++ appdiff.sh
@@ -12,7 +12,7 @@
 cd /root/indoorpy
 
 
-VER_REMOTE=`git ls-remote https://github.com/isra67/indoorjs.git | grep master | cut -f 1`
+VER_REMOTE=`git ls-remote https://github.com/isra67/indoorpy.git | grep master | cut -f 1`
 GITDIFF=`git cherry -v | grep $VER_REMOTE`
 
 if [ "$GITDIFF" != "" ]
